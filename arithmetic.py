d89de4e74b1ea79f234c43eed470ffe9600543d7
Renice-Owino <reniceo18@gmail.com>
2019-07-24 14:05:17 +0300

    added the var and arithmetic files

---
+++ arithmetic.py
@@ -0,0 +1,37 @@
+# Udemy arithmetic operator tuitorial.
+age1 = 23
+age2 = 25
+
+# Sum
+sum = age1 + age2
+print sum
+
+# Diff
+Diff = age2 -age1
+print Diff
+
+# Multiply
+Mult = age1*age2
+print Mult
+
+# Division
+div = age2%age1
+print div
+
+# modulo
+mod = age2%age1
+print mod
+
+# adding strings
+firstname = "Renice"
+lastname = "Owino"
+
+name = firstname +" "+ lastname
+print name
+
+# printing characters of a sentence
+sentence = "Renice loves python"
+print sentence[0]
+
+# Splicing
+print sentence[0:6]
---
+++ var.py
@@ -0,0 +1,16 @@
+# Udemy var tuitorial.
+print("Hello World")
+age = 20
+sent = "My name is Jayjay"
+
+# multiple assignment
+sarah, bob, mike =16, 21, 17
+
+# Assignment
+sarah = bob = mike = 17
+
+#create a multiple assignment that has your name and age:
+name, age = "My name is Rhe", 24
+
+print name
+print age
